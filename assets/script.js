757d94dd87a7f5ef6e2b01628eed378b4c4946c7
Timiwade97 <timiwade97@gmail.com>
2023-08-06 17:35:47 +0100

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,71 @@
 // Wait for the page to load before executing the script
 document.addEventListener("DOMContentLoaded", () => {
-    // Replace YOUR_INFURA_API_KEY with the API key you obtained from Infura
-    const infuraApiKey = 'bc04eb06653141e08d69793defb2837b';
-    const provider = `https://mainnet.infura.io/v3/${infuraApiKey}`;
-  
-    // Initialize Web3 with the Infura provider
-    const web3 = new Web3(new Web3.providers.HttpProvider(provider));
-  
-    const resultsPerPage = 10; // Number of results to display per page
-    let currentPage = 1;
-    let totalTransactions = 0;
-    let pendingTransactions = [];
-  
-    // Function to fetch mempool data and update the table
-    async function fetchMempoolData() {
-      try {
-        // Get the current block number
-        const blockNumber = await web3.eth.getBlockNumber();
-  
-        // Get the pending transactions using the current block number
-        const blockData = await web3.eth.getBlock(blockNumber, true);
-  
-        // Get the pending transactions from the block data
-        pendingTransactions = blockData.transactions;
-        totalTransactions = pendingTransactions.length;
-  
-        // Sort transactions by timestamp in descending order (most recent first)
-        pendingTransactions.sort((a, b) => b.timestamp - a.timestamp);
-  
-        // Update the table with the current page data
-        updateTable();
-      } catch (error) {
-        console.error('Error fetching mempool data:', error);
-      }
+  // Replace YOUR_INFURA_API_KEY with the API key you obtained from Infura
+  const infuraApiKey = 'bc04eb06653141e08d69793defb2837b';
+  const provider = `https://mainnet.infura.io/v3/${infuraApiKey}`;
+
+  // Initialize Web3 with the Infura provider
+  const web3 = new Web3(new Web3.providers.HttpProvider(provider));
+
+  const resultsPerPage = 10; // Number of results to display per page
+  let currentPage = 1;
+  let totalTransactions = 0;
+  let pendingTransactions = [];
+
+  // Function to show the loading message and hide the mempool data table
+  function showLoadingMessage() {
+    const loadingMessageElement = document.getElementById('loading-message');
+    const mempoolDataElement = document.getElementById('mempool-data');
+    loadingMessageElement.style.display = 'block';
+    mempoolDataElement.style.display = 'none';
+  }
+
+  // Function to hide the loading message and show the mempool data table
+  function hideLoadingMessage() {
+    const loadingMessageElement = document.getElementById('loading-message');
+    const mempoolDataElement = document.getElementById('mempool-data');
+    loadingMessageElement.style.display = 'none';
+    mempoolDataElement.style.display = 'block';
+  }
+
+  // Show the loading message while fetching data
+  showLoadingMessage();
+
+  // Function to fetch mempool data and update the table
+  async function fetchMempoolData() {
+    try {
+      // Get the current block number
+      const blockNumber = await web3.eth.getBlockNumber();
+
+      // Get the pending transactions using the current block number
+      const blockData = await web3.eth.getBlock(blockNumber, true);
+
+      // Get the pending transactions from the block data
+      pendingTransactions = blockData.transactions;
+      totalTransactions = pendingTransactions.length;
+
+      // Sort transactions by timestamp in descending order (most recent first)
+      pendingTransactions.sort((a, b) => b.timestamp - a.timestamp);
+
+      // Update the table with the current page data
+      updateTable();
+
+      // Hide the loading message when the data is ready
+      hideLoadingMessage();
+    } catch (error) {
+      console.error('Error fetching mempool data:', error);
+      // Hide the loading message in case of an error
+      hideLoadingMessage();
     }
-  
-    // Function to update the table with the current page data
-    function updateTable() {
-      const startIndex = (currentPage - 1) * resultsPerPage;
-      const endIndex = Math.min(startIndex + resultsPerPage, totalTransactions);
-  
-      // Create the table header
-      let tableHTML = `
+  }
+
+  // Function to update the table with the current page data
+  function updateTable() {
+    const startIndex = (currentPage - 1) * resultsPerPage;
+    const endIndex = Math.min(startIndex + resultsPerPage, totalTransactions);
+
+    // Create the table header
+    let tableHTML = `
         <table>
           <thead class="tbl-header">
             <tr>
@@ -54,11 +78,11 @@ document.addEventListener("DOMContentLoaded", () => {
           </thead>
           <tbody>
       `;
-  
-      // Populate the table rows with the current page data
-      for (let i = startIndex; i < endIndex; i++) {
-        const tx = pendingTransactions[i];
-        tableHTML += `
+
+    // Populate the table rows with the current page data
+    for (let i = startIndex; i < endIndex; i++) {
+      const tx = pendingTransactions[i];
+      tableHTML += `
           <tr>
             <td>${tx.hash}</td>
             <td>${tx.from}</td>
@@ -67,66 +91,66 @@ document.addEventListener("DOMContentLoaded", () => {
             <td>${tx.gasPrice}</td>
           </tr>
         `;
-      }
-  
-      // Close the table
-      tableHTML += '</tbody></table>';
-  
-      // Display the mempool data table on the website
-      const mempoolDataElement = document.getElementById('mempool-data');
-      mempoolDataElement.innerHTML = tableHTML;
-  
-      // Update the navigation buttons and page number display
-      updateNavigationButtons();
     }
-  
-    // Function to update the navigation buttons and page number display
-    function updateNavigationButtons() {
-      const prevButton = document.getElementById('prev-button');
-      const nextButton = document.getElementById('next-button');
-      const pageNumber = document.getElementById('page-number');
-      const totalPages = Math.ceil(totalTransactions / resultsPerPage);
-  
-      prevButton.disabled = currentPage === 1;
-      nextButton.disabled = currentPage === totalPages;
-      pageNumber.textContent = `Page ${currentPage} / ${totalPages}`;
-    }
-  
-    // Function to navigate to the previous page
-    function goToPreviousPage() {
-      if (currentPage > 1) {
-        currentPage--;
-        updateTable();
-      }
-    }
-  
-    // Function to navigate to the next page
-    function goToNextPage() {
-      const totalPages = Math.ceil(totalTransactions / resultsPerPage);
-      if (currentPage < totalPages) {
-        currentPage++;
-        updateTable();
-      }
-    }
-  
-    // Add event listeners to the navigation buttons
+
+    // Close the table
+    tableHTML += '</tbody></table>';
+
+    // Display the mempool data table on the website
+    const mempoolDataElement = document.getElementById('mempool-data');
+    mempoolDataElement.innerHTML = tableHTML;
+
+    // Update the navigation buttons and page number display
+    updateNavigationButtons();
+  }
+
+  // Function to update the navigation buttons and page number display
+  function updateNavigationButtons() {
     const prevButton = document.getElementById('prev-button');
     const nextButton = document.getElementById('next-button');
-    prevButton.addEventListener('click', goToPreviousPage);
-    nextButton.addEventListener('click', goToNextPage);
-  
-    // Fetch initial mempool data and display the table
-    fetchMempoolData();
-  
-    // Update the mempool data every 2 seconds
-    setInterval(fetchMempoolData, 2000);
-  });
-  
-  
-  
-  
-  
-  
+    const pageNumber = document.getElementById('page-number');
+    const totalPages = Math.ceil(totalTransactions / resultsPerPage);
+
+    prevButton.disabled = currentPage === 1;
+    nextButton.disabled = currentPage === totalPages;
+    pageNumber.textContent = `Page ${currentPage} / ${totalPages}`;
+  }
+
+  // Function to navigate to the previous page
+  function goToPreviousPage() {
+    if (currentPage > 1) {
+      currentPage--;
+      updateTable();
+    }
+  }
+
+  // Function to navigate to the next page
+  function goToNextPage() {
+    const totalPages = Math.ceil(totalTransactions / resultsPerPage);
+    if (currentPage < totalPages) {
+      currentPage++;
+      updateTable();
+    }
+  }
+
+  // Add event listeners to the navigation buttons
+  const prevButton = document.getElementById('prev-button');
+  const nextButton = document.getElementById('next-button');
+  prevButton.addEventListener('click', goToPreviousPage);
+  nextButton.addEventListener('click', goToNextPage);
+
+  // Fetch initial mempool data and display the table
+  fetchMempoolData();
+
+  // Update the mempool data every 10 seconds
+  setInterval(fetchMempoolData, 10000);
+});
+
+
+
+
+
+
 
 //   API_KEY
 //   bc04eb06653141e08d69793defb2837b
